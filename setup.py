faade420dbe49815cc1082ccd749b4e5874f276d
Craig Hobbs <craigahobbs@gmail.com>
2019-02-14 06:27:48 -0800

    bump version to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='unittest-parallel',
-    version='0.3',
+    version='0.4',
     author='Craig Hobbs',
     author_email='craigahobbs@gmail.com',
     description=('Parallel unittest runner.'),
