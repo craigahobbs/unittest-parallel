80b03ace40fa206ff456175a3acc3014157d1f3e
Craig Hobbs <craigahobbs@gmail.com>
2019-02-24 01:17:38 -0800

    re-organize source code

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 # Licensed under the MIT License
 # https://github.com/craigahobbs/unittest-parallel/blob/master/LICENSE
 
+import re
 import os
 
 from setuptools import setup
 
-import unittest_parallel
+PACKAGE_NAME = 'unittest-parallel'
+MODULE_NAME = 'unittest_parallel'
 
 TESTS_REQUIRE = [
     'coverage'
 ]
 
 def main():
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'src', MODULE_NAME, '__init__.py'), encoding='utf-8') as init_file:
+        version = re.search(r"__version__ = '(.+?)'", init_file.read()).group(1)
     with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as readme_file:
         long_description = readme_file.read()
 
     setup(
-        name='unittest-parallel',
-        version=unittest_parallel.__version__,
+        name=PACKAGE_NAME,
+        description=('Parallel unit test runner for Python3 with coverage support'),
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        version=version,
         author='Craig Hobbs',
         author_email='craigahobbs@gmail.com',
         keywords='unittest parallel',
         url='https://github.com/craigahobbs/unittest-parallel',
         license='MIT',
-        description=('Parallel unit test runner for Python3 with coverage support'),
-        long_description=long_description,
-        long_description_content_type='text/markdown',
         classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
@@ -37,12 +41,12 @@ def main():
             "Topic :: Software Development :: Testing",
             "Topic :: Utilities",
         ],
-        packages=['unittest_parallel'],
-        install_requires=[],
+        package_dir={'': 'src'},
+        packages=[MODULE_NAME],
         entry_points={
-            'console_scripts': ['unittest-parallel = unittest_parallel.main:main'],
+            'console_scripts': [PACKAGE_NAME + ' = ' + MODULE_NAME + '.main:main'],
         },
-        test_suite='unittest_parallel.tests',
+        test_suite='tests',
         tests_require=TESTS_REQUIRE,
         extras_require={
             'tests': TESTS_REQUIRE
