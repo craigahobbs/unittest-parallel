a1c68c4e35752b1bf8c08321a0f767aac5f1bb08
craigahobbs@gmail.com <Craig A. Hobbs>
2022-04-10 09:21:10 -0700

    unittest-parallel 1.5.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def main():
         description='Parallel unit test runner with coverage support',
         long_description=long_description,
         long_description_content_type='text/markdown',
-        version='1.5.0',
+        version='1.5.1',
         author='Craig A. Hobbs',
         author_email='craigahobbs@gmail.com',
         keywords='test unittest coverage parallel',
